dc6bfca2d498fa51ba6dd9b67b9c1f73102693f0
Marjose <hypernate1@gmail.com>
2023-01-17 12:02:27 +0800

    WIP

---
+++ src/Observers/ModelObserver.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Marjose123\FilamentWebhookServer\Observers;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class ModelObserver
+{
+    public function created(?Model $model)
+    {
+
+    }
+
+    public function updated(?Model $model)
+    {
+
+    }
+
+    public function deleted(?Model $model)
+    {
+    }
+
+    public function restored(?Model $model)
+    {
+    }
+
+    public function forceDeleted(?Model $model)
+    {
+    }
+}
