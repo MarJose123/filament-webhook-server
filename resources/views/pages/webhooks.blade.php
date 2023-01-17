e752d4a8ce8a8ccdc1a76d7341f415f5cf15c9e8
Marjose <hypernate1@gmail.com>
2023-01-17 16:31:39 +0800

    added tables

--- resources/views/pages/webhooks.blade.php
+++ resources/views/pages/webhooks.blade.php
@@ -1,11 +1,14 @@
 <x-filament::page>
-
+    <div>
+        {{ $this->table }}
+    </div>
     <x-forms::modal id="create-webhook" width="lg">
         <x-slot name="subheading">
             <h3 class="text-xl">{{ __('filament-webhook-server::default.pages.modal.label') }}</h3>
         </x-slot>
         {{$this->form}}
-        <x-slot name="actions">
+
+        <x-slot name="actions" style="padding-top: 40px;">
             <x-filament::modal.actions>
                 <x-filament::button wire:click="create" color="primary">
                     {{ __('filament-webhook-server::default.pages.modal.button.create') }}
