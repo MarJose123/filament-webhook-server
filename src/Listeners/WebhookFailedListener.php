37a4ee926ed824c0e8549337d041ae835a9bca9a
Marjose <hypernate1@gmail.com>
2023-02-13 17:29:15 +0800

    Fixed Event Listener

--- src/Listeners/WebhookFailedListener.php
+++ src/Listeners/WebhookFailedListener.php
@@ -3,32 +3,28 @@
 namespace Marjose123\FilamentWebhookServer\Listeners;
 
 use Illuminate\Support\Facades\Log;
-use Marjose123\FilamentWebhookServer\Models\FilamentWebhookServer;
 use Marjose123\FilamentWebhookServer\Models\FilamentWebhookServerHistory;
-use Spatie\WebhookServer\Events\WebhookCallSucceededEvent;
+use Spatie\WebhookServer\Events\WebhookCallFailedEvent;
 
-class WebhookSuccessListener
+class WebhookFailedListener
 {
     public function __construct()
     {
     }
 
-    public function handle(WebhookCallSucceededEvent $event)
+    public function handle(WebhookCallFailedEvent $event)
     {
         try {
             $webhookClientHistory = new FilamentWebhookServerHistory();
             $webhookClientHistory->webhook_client = $event->meta['webhookClient'];
             $webhookClientHistory->uuid = $event->uuid;
             $webhookClientHistory->status_code = $event->response->getStatusCode();
-            $webhookClientHistory->errorMessage = "";
-            $webhookClientHistory->errorType = "";
+            $webhookClientHistory->errorMessage = $event->response->getReasonPhrase();
+            $webhookClientHistory->errorType = $event->errorType;
             $webhookClientHistory->attempt = $event->attempt;
             $res = $webhookClientHistory->save();
         }catch (\Exception $error){
             Log::info(print_r($error, true));
         }
-
-
-
     }
 }
