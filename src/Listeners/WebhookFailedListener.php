183daa47969ad5060d835cb0ec563d74cd593c1b
Marjose <hypernate1@gmail.com>
2023-02-13 17:31:25 +0800

    Fixed Event Listener

--- src/Listeners/WebhookFailedListener.php
+++ src/Listeners/WebhookFailedListener.php
@@ -14,17 +14,19 @@ class WebhookFailedListener
 
     public function handle(WebhookCallFailedEvent $event)
     {
-        try {
-            $webhookClientHistory = new FilamentWebhookServerHistory();
-            $webhookClientHistory->webhook_client = $event->meta['webhookClient'];
-            $webhookClientHistory->uuid = $event->uuid;
-            $webhookClientHistory->status_code = $event->response->getStatusCode();
-            $webhookClientHistory->errorMessage = $event->response->getReasonPhrase();
-            $webhookClientHistory->errorType = $event->errorType;
-            $webhookClientHistory->attempt = $event->attempt;
-            $res = $webhookClientHistory->save();
-        }catch (\Exception $error){
-            Log::info(print_r($error, true));
+        if(config('filament-webhook-server.webhook.keep_history')){
+            try {
+                $webhookClientHistory = new FilamentWebhookServerHistory();
+                $webhookClientHistory->webhook_client = $event->meta['webhookClient'];
+                $webhookClientHistory->uuid = $event->uuid;
+                $webhookClientHistory->status_code = $event->response->getStatusCode();
+                $webhookClientHistory->errorMessage = $event->response->getReasonPhrase();
+                $webhookClientHistory->errorType = $event->errorType;
+                $webhookClientHistory->attempt = $event->attempt;
+                $res = $webhookClientHistory->save();
+            }catch (\Exception $error){
+                Log::info(print_r($error, true));
+            }
         }
     }
 }
--- src/Listeners/WebhookSuccessListener.php
+++ src/Listeners/WebhookSuccessListener.php
@@ -15,18 +15,21 @@ class WebhookSuccessListener
 
     public function handle(WebhookCallSucceededEvent $event)
     {
-        try {
-            $webhookClientHistory = new FilamentWebhookServerHistory();
-            $webhookClientHistory->webhook_client = $event->meta['webhookClient'];
-            $webhookClientHistory->uuid = $event->uuid;
-            $webhookClientHistory->status_code = $event->response->getStatusCode();
-            $webhookClientHistory->errorMessage = "";
-            $webhookClientHistory->errorType = "";
-            $webhookClientHistory->attempt = $event->attempt;
-            $res = $webhookClientHistory->save();
-        }catch (\Exception $error){
-            Log::info(print_r($error, true));
+        if(config('filament-webhook-server.webhook.keep_history')){
+            try {
+                $webhookClientHistory = new FilamentWebhookServerHistory();
+                $webhookClientHistory->webhook_client = $event->meta['webhookClient'];
+                $webhookClientHistory->uuid = $event->uuid;
+                $webhookClientHistory->status_code = $event->response->getStatusCode();
+                $webhookClientHistory->errorMessage = "";
+                $webhookClientHistory->errorType = "";
+                $webhookClientHistory->attempt = $event->attempt;
+                $res = $webhookClientHistory->save();
+            }catch (\Exception $error){
+                Log::info(print_r($error, true));
+            }
         }
+
 
 
 
