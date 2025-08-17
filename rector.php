1c834019e3f129ff097acd270e3dbc8df70448c6
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-08-17 16:55:20 +0800

    style: Adjust Rector path spacing

--- rector.php
+++ rector.php
@@ -6,10 +6,10 @@ use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/resources',
-        __DIR__ . '/routes',
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
+        __DIR__.'/resources',
+        __DIR__.'/routes',
+        __DIR__.'/src',
+        __DIR__.'/tests',
     ])
     // uncomment to reach your current PHP version
     // ->withPhpSets()
