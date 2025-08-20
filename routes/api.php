97a3f24b618ce2d672c7f5c51ca5e6817e2011e4
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-08-20 11:41:19 +0800

    feat: Add Sanctum authentication to API routes

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Marjose123\FilamentWebhookServer\Http\Controllers\WebhookController;
 use Marjose123\FilamentWebhookServer\WebhookPlugin;
 
 if (WebhookPlugin::get()->isEnabled() && WebhookPlugin::get()->isApiRoutesEnabled()) {
-    Route::prefix('/webhook-server/api')->middleware('web')->group(function (): void {
+    Route::prefix('/webhook-server/api')->middleware(['web', 'auth:sanctum'])->group(function (): void {
         Route::get('/', [WebhookController::class, 'get']);
         Route::post('/', [WebhookController::class, 'create']);
         Route::patch('/{id}', [WebhookController::class, 'update']);
