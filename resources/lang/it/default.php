f8a502f4c2b1b3f1af3f84d7a6dadf422b03e94a
Marco Germani <info@marcogermani.it>
2026-05-21 11:45:03 +0200

    Added IT translation
    Added IT translation

---
+++ resources/lang/it/default.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'pages' => [
+        'heading' => 'Webhook',
+        'navigation' => [
+            'label' => 'Webhook',
+            'group' => 'Sviluppatore',
+        ],
+        'button' => [
+            'add_new_webhook' => 'Crea Webhook',
+        ],
+        'modal' => [
+            'label' => 'Crea Webhook',
+            'button' => [
+                'create' => 'Crea',
+            ],
+        ],
+        'history' => [
+            'heading' => 'Webhook Log',
+            'back' => 'Indietro',
+        ],
+    ],
+    'notification' => [
+        'create' => [
+            'success' => 'Webhook creato con successo!',
+        ],
+    ],
+
+];
